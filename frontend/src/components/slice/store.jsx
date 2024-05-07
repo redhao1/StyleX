3f795d85f9c743263e8397d0b31d6b5bbb4308a6
Somil Saparia <sapariasomil@gmail.com>
2024-05-06 21:24:55 -0400

    All documents completed

--- frontend/src/components/slice/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../Auth/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
